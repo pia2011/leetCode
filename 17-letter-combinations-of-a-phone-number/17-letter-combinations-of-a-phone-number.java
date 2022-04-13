9e7d6d777f43cd4c142b5b0d1d6b129a44b29408
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-13 21:56:53 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (94.65%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,26 @@
 import java.util.*;
 
 class Solution {
+    static final String[] mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<>();
-        if(digits.isEmpty()) return ans;
+        List<String> ans = new LinkedList<>();
+        if(digits.length() == 0) return ans;
         
-        String mapping[] = new String[]{"0", "1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        
-        ans.add(0,"");
-        
-        for(int i = 0; i<digits.length(); i++){
-            int x = digits.charAt(i) - '0';
-            int size = ans.size();
-            for(int k = 1; k<=size; k++){ 
-                String tmp = ans.remove(0); // 앞 부터 이전 단계의 것들 모조리 
-                for(char c : mapping[x].toCharArray()){
-                    ans.add(tmp + c); // 뒤에 추가
-                }
-            }
+        combination(ans, digits, 0, new StringBuilder());
+        return ans;
+    }
+    
+    private void combination(List<String> ans,String digits, int order, StringBuilder sb){ // 백트래킹
+        if(order == digits.length()){ // 범위를 넘어서면 종료 
+            ans.add(new String(sb));
+            return;
         }
         
-        return ans;
+        String letter = mapping[digits.charAt(order) - '0'];
+        for(int i = 0; i<letter.length(); i++){
+            sb.append(letter.charAt(i));
+            combination(ans, digits, order + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
