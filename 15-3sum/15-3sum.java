c41825fd5abc96a68a9a0cd6648f888586b9a26c
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-16 20:27:42 +0900

    Time: 620 ms (15.68%), Space: 46.6 MB (87.18%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 3개의 합이 0을 이루는 조합을 뱉어내자. 중복 경우는 제외
-        HashSet<List<Integer>> ans = new HashSet<>();
+        
         Arrays.sort(nums);
- 
-        // 0 0 0 0 0 0 | 0 0
+        HashSet<List<Integer>> ans = new HashSet<>(); // 중복 경우 제외 
+        
         for(int i = 0; i<nums.length - 2; i++){
-            // idx
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0) ans.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
-                else if(sum < 0) j++;
-                else if(sum > 0) k--;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) ans.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                else if(sum < 0) left++;
+                else if(sum > 0) right--;
             }
         }
-        
+
         return new ArrayList<>(ans);
     }
 }
