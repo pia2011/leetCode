fc0cdbba5b822af1d050644f8ffa44095f0eb1fa
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-26 01:28:42 +0900

    Time: 0 ms (100.00%), Space: 49 MB (6.10%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,15 +1,11 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        // 대각선의 모든 합을 반환 : i == j || i == (m - 1 - j)
-        int ans = 0;
+        // 대각선의 모든 합을 반환 : i == j || i == (m - 1 - j) - case 1
         
+        int ans = 0;
+        if(mat.length % 2 != 0) ans -= mat[mat.length/2][mat.length/2]; // remove overlap
         for(int i = 0; i<mat.length; i++){
-            for(int j = 0; j<mat[0].length; j++){
-                 if(i == j || i == mat[0].length - 1 - j){
-                     ans += mat[i][j];
-                 }
-            }
-            
+            ans += mat[i][i] + mat[i][mat[0].length - 1 - i];
         }
         
         return ans;
