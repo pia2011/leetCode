e10cd1a041efeda18beea8da97cd62351b6e6154
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-27 03:46:37 +0900

    Time: 8 ms (62.01%), Space: 46 MB (39.94%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String intToRoman(int num) {
+        // 예외 고려 : 4,9로 시작하는 수
+        StringBuilder sb = new StringBuilder();
+        int p[] = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String r[] = new String[]{"M", "CM", "D", "CD", "C", "XC","L","XL", "X", "IX", "V", "IV", "I"};
+        
+        for(int i = 0; i<p.length; i++){
+            if(i%2 == 0 && num/p[i] > 0){
+                while(num/p[i] != 0){
+                    sb.append(r[i]);
+                    num -= p[i];
+                }
+            }else if(i%2 != 0 && num/p[i] == 1){ // 배수 적용 안됨 : only 1
+                sb.append(r[i]);
+                num -= p[i];
+            }
+        }
+        
+        return sb.toString();
+    }
+}
