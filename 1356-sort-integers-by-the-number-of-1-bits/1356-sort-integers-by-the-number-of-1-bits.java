f7c1aee038adf46a49a0b364da2425e0e69f1fdd
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-28 01:03:52 +0900

    Time: 36 ms (10.67%), Space: 51.5 MB (5.08%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        // 2진수로 바꿨을 때 갖는 비트의 수를 기준으로 오름차순 정렬하기
+
+        LinkedList<Point> list = new LinkedList<>();
+        for(int i = 0; i<arr.length; i++){
+            String s = Integer.toBinaryString(arr[i]);
+            int cnt = (int)s.chars().filter(c -> c == '1').count();
+            list.add(new Point(arr[i], cnt));
+        } 
+        
+        Collections.sort(list);
+        
+        int ans[] = new int[list.size()];
+        for(int i = 0; i<list.size(); i++){
+            ans[i] = list.get(i).num;
+        }
+        
+        return ans;
+    }
+   
+     static class Point implements Comparable<Point>{
+        int num, cnt;
+        
+        Point(int num, int cnt){
+            this.num = num;
+            this.cnt = cnt;
+        }
+        
+        @Override
+        public int compareTo(Point o){
+            if(cnt == o.cnt){
+                return num - o.num;
+            }else return cnt - o.cnt;
+        }
+        
+    }
+}
