76ba887bb9257cf707af3462d4fc06f41a1ff686
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-04-30 21:30:11 +0900

    Time: 73 ms (52.71%), Space: 102 MB (41.33%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -1,44 +1,37 @@
 class Solution {
     
-    static int parent[];
+    static int[] parent;
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
         // 가능한 오름차순에 근접하도록 재배치하여 반환
-        
+       
         init(s.length());
         
-        for(int i = 0; i<pairs.size(); i++){
-            List<Integer> p = pairs.get(i);
-            int a = p.get(0);
-            int b = p.get(1);
-            
-            union(a,b);
+        for(List<Integer> pair : pairs){
+            union(pair.get(0), pair.get(1));
         }
         
         HashMap<Integer, List<Integer>> groups = new HashMap<>();
+       
         for(int i = 0; i<s.length(); i++){
             int root = find(i);
-            groups.putIfAbsent(root, new ArrayList<>());
+            groups.putIfAbsent(root , new ArrayList());
             groups.get(root).add(i);
         }
         
         char ans[] = new char[s.length()];
-        for(List<Integer> values : groups.values()){
-            List<Character> tmpList = new ArrayList<>();
-            for(int idx : values){
-                tmpList.add(s.charAt(idx));
+        for(List<Integer> group : groups.values()){
+            List<Character> tempList = new ArrayList<>();
+            for(int x : group){
+                tempList.add(s.charAt(x));
             }
+            Collections.sort(tempList);
             
-            Collections.sort(tmpList);
-            
-            for(int i = 0; i<values.size(); i++){
-                ans[values.get(i)] = tmpList.get(i);
+            for(int i = 0; i<group.size(); i++){
+                ans[group.get(i)] = tempList.get(i);
             }
         }
-
-        
         
         return new String(ans);
-        
     }
     static void init(int len){
         parent = new int[len];
@@ -46,26 +39,23 @@ class Solution {
             parent[i] = -1;
         }
     }
-    
     static void union(int a, int b){
         a = find(a);
         b = find(b);
         
-        if(a == b) return; // already in same group
+        if(a == b) return;
         
-        // compare height (둘 다 음수 -> 높이가 클 수록 작음)
-        if(parent[a] < parent[b]){
-            parent[a] += parent[b]; 
+        if(parent[a] < parent[b]){ // 둘 다 음수
+            parent[a] += parent[b];
             parent[b] = a;
         }else{
             parent[b] += parent[a];
             parent[a] = b;
         }
     }
-    
     static int find(int x){
         if(parent[x] < 0) return x;
-        else {
+        else{
             int p = find(parent[x]);
             parent[x] = p;
             return p;
