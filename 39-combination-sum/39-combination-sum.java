036b1a576f5d1772146ae8ad6eb04ab2f2f470ca
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-04 03:10:06 +0900

    Time: 29 ms (6.37%), Space: 48.4 MB (11.07%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        // target을 만들기 위한 조합(순서 상관 없음)
+        
+        List<List<Integer>> ansList = new LinkedList<>();
+        
+        backTracking(candidates, target, ansList,0,0, new boolean[candidates.length], new LinkedList<>());
+        
+        return ansList; 
+        
+    }
+    
+    static void backTracking(int[] candidates,int target,List<List<Integer>> ansList,int start, int sum, boolean visited[], LinkedList<Integer> list){
+        
+        if(sum > target) return;
+        if(sum == target) {
+            System.out.println("성공");
+            ansList.add(new LinkedList<>(list));
+            return;
+        }
+        
+        
+        for(int i = start; i<candidates.length; i++){
+
+
+            list.add(candidates[i]);
+            backTracking(candidates, target, ansList, i,sum + candidates[i], visited, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
