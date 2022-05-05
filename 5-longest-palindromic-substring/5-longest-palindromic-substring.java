dbc264a64165a358ba38ac0eba16d03c8020e8d9
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-06 02:48:16 +0900

    Time: 42 ms (66.11%), Space: 43.2 MB (62.46%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,18 +1,17 @@
-public class Solution {
-private int lo, maxLen;
-
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
+class Solution {
+    private int lo, maxLen;
+    public String longestPalindrome(String s) {
+        // 가장 긴 팰린드롭 문자열 반환
+        int len = s.length();
+        if(s.length() <= 1) return s; 
+        
     for (int i = 0; i < len-1; i++) {
      	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
      	extendPalindrome(s, i, i+1); //assume even length.
     }
     return s.substring(lo, lo + maxLen);
-}
-
+    }
+    
 private void extendPalindrome(String s, int j, int k) {
 	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
 		j--;
