fce396c492ac6baf3f4a45ed084657e6d90ed49d
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-07 13:45:57 +0900

    Time: 9 ms (6.84%), Space: 44.9 MB (89.18%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -19,34 +19,26 @@ class Solution {
     
     private static int bfs(int cost[][]){
         
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0,0));
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
         dist[0][0] = cost[0][0];
         
         while(!q.isEmpty()){
-            Point now = q.poll();
+            int[] now = q.poll();
             for(int i = 0; i<2; i++){
-                int nx = dx[i] + now.x;
-                int ny = dy[i] + now.y;
+                int nx = dx[i] + now[0];
+                int ny = dy[i] + now[1];
                 
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if(dist[now.x][now.y] + cost[nx][ny] >= dist[nx][ny]) continue;
+                if(dist[now[0]][now[1]] + cost[nx][ny] >= dist[nx][ny]) continue;
                 
-                dist[nx][ny] = dist[now.x][now.y] + cost[nx][ny];
-                q.add(new Point(nx,ny));
+                dist[nx][ny] = dist[now[0]][now[1]] + cost[nx][ny];
+                q.add(new int[]{nx,ny});
             }
         }
         
         return dist[n - 1][m - 1];
     }
-    
-    
-    static class Point{
-        int x,y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+
 }
 
