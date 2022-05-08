d8421029238b31dfb90d972a90e20add383092ed
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-09 02:29:34 +0900

    Time: 5 ms (81.01%), Space: 43.4 MB (30.43%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,11 +1,33 @@
-import java.math.*;
-
 class Solution {
     public String multiply(String num1, String num2) {
+        int n = num1.length(); 
+        int m = num2.length();
+        int result[] = new int[n + m];
         
-        BigInteger a = new BigInteger(num1);
-        BigInteger b = new BigInteger(num2);
+        // 0이 발생하는 경우
+        if(num1.equals("0") || num2.equals("0")) return "0";
         
-        return a.multiply(b).toString();
+        if(n == 1 && m == 1) {
+            return String.valueOf(Integer.parseInt(num1) * Integer.parseInt(num2));
+        } 
+        
+        for(int i = n - 1; i>=0; i--){
+            for(int j = m - 1; j>=0; j--){
+                
+                int temp = result[i + j + 1]; // 원래 자리 값
+                temp += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                
+                result[i + j + 1] = temp % 10;
+                result[i + j] += temp / 10; // 올림 수
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int x : result){
+            if(sb.length() == 0 && x == 0) continue; // ex> 2 * 3
+            sb.append(x);
+        }
+        
+        return sb.toString();
     }
 }
