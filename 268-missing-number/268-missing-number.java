6d64ef479e94007e72cd1f2af84fc7ff20d1dcda
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-08 17:27:22 +0900

    Time: 0 ms (100.00%), Space: 51.8 MB (15.75%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,16 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        int ans = -1;
-        for(int i = 0; i<n; i++){
-            if(nums[i] != i){ 
-                ans = i;
-                break;
-            }
-        }
-        if(ans == -1) ans = n;
         
-        return ans;
+        int n = nums.length;
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        
+        return (n * (n + 1))/2 - sum;
     }
 }
