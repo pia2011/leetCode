35082cbc048e32a5b14f41ed5c40437d8f466f89
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-10 21:56:12 +0900

    Time: 22 ms (77.21%), Space: 79.6 MB (53.16%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -1,20 +1,10 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // 정렬 사용 불가
         
-        HashMap<Integer, List<Integer>> hm = new HashMap<>();
+        Set<Integer> hs = new HashSet<>();
         for(int i = 0; i<nums.length; i++){
-            hm.putIfAbsent(nums[i], new LinkedList<>());
-            hm.get(nums[i]).add(i);
-        }
-        
-        for(int num : hm.keySet()){
-            List<Integer> list = hm.get(num);
-            Collections.sort(list);
-            for(int i = 1; i<list.size(); i++){
-                int dist = list.get(i) - list.get(i - 1);
-                if(dist <= k) return true;
-            }
+            if(i > k) hs.remove(nums[i - k - 1]);
+            if(!hs.add(nums[i])) return true; // 없으면 넣고 있으면 return true;
         }
         
         return false;
