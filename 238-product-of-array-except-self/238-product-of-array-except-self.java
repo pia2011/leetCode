f31d8239a10f39689e915d88f302499d9d523d5d
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-10 22:52:22 +0900

    Time: 4 ms (16.20%), Space: 57.7 MB (49.00%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // O(N)안에 처리..?
+        int a = 1;
+        boolean zero1 = false;
+        boolean zero2 = false;
+        boolean isZero[] = new boolean[nums.length];
+        for(int i = 0; i<nums.length; i++){
+            
+            if(nums[i] == 0){   
+                isZero[i] = true;
+                if(zero1) {
+                    zero2 = true;
+                    zero1 = false;
+                    continue;
+                }
+                if(!zero2) zero1 = true;
+            }
+            
+            a *= nums[i];
+        }
+        int temp = 1;
+        if(zero1){
+            for(int i = 0; i<nums.length; i++){
+                if(!isZero[i])
+                    temp *= nums[i];
+            }
+        }
+    
+        int ans[] = new int[nums.length];
+        if(zero1) System.out.println(1);
+        if(zero2) System.out.println(2);
+        for(int i = 0; i<nums.length; i++){
+            if(zero1){
+                if(isZero[i]){
+                    ans[i] = temp;
+                }else{
+                    ans[i] = 0;
+                }
+            }else if(zero2){
+                ans[i] = 0;
+            }else{
+                ans[i] = a / nums[i];    
+            }
+            
+        }
+        
+        return ans;
+    }
+}
