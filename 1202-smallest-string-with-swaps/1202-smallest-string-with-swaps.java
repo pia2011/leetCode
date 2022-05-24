2a351ea977d7988a5a147aa03bbb048badfa4a53
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-24 16:50:24 +0900

    Time: 54 ms (77.17%), Space: 102.5 MB (37.08%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -1,68 +1,61 @@
 class Solution {
     static int parent[];
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        
-        // 스왑을 사용해서 S를 사전순으로 가장 작은 단어로 만든다. 스왑의 횟수는 무제한이다.
-        // 같은 그룹 사이에서는 어떻게든 이동할 수 있다. -> 분리집합을 사용해서 묶어준다.
-        
-        // step1. 자기 자신을 route로 만들어준다.
+     
         parent = new int[s.length()];
         for(int i = 0; i<s.length(); i++){
             parent[i] = -1;
-        }        
+        }
         
-        // step2. 
-        for(int i = 0; i<pairs.size(); i++){
-            List<Integer> temp = pairs.get(i);
-            int a = temp.get(0);
-            int b = temp.get(1);
+        for(List<Integer> pair : pairs){
+            int a = pair.get(0);
+            int b = pair.get(1);
             union(a,b);
         }
         
-        // step3. 그룹별로 묶어주기
         HashMap<Integer, List<Integer>> groups = new HashMap<>();
-        for(int i = 0; i<s.length();i++){
+        for(int i = 0; i<s.length(); i++){
             int root = find(i);
-            groups.putIfAbsent(root, new ArrayList<>());
+            groups.putIfAbsent(root , new ArrayList<>());
             groups.get(root).add(i);
-        }
+        }    
         
         char ans[] = new char[s.length()];
-        for(List<Integer> values : groups.values()){
+        for(List<Integer> idxGroup : groups.values()){
             List<Character> tempList = new ArrayList<>();
-            for(int x : values){
-                tempList.add(s.charAt(x));
+            for(int idx : idxGroup){
+                tempList.add(s.charAt(idx));
             }
-           
+            
             Collections.sort(tempList);
             
-            for(int i = 0; i<values.size(); i++){
-                ans[values.get(i)] = tempList.get(i);
+            for(int i = 0; i<tempList.size(); i++){
+                ans[idxGroup.get(i)] = tempList.get(i);
             }
         }
         
         return new String(ans);
     }
     
-    static void union(int a, int b){
+    private static void union(int a, int b){
         a = find(a);
         b = find(b);
-        if(a == b) return;
-            
         
-        if(parent[a] < parent[b]){
-            parent[a] += parent[b]; // 개수 추가
-            parent[b] = a; // 부모 변경 
-        }else{
+        if(a == b) return; // 이미 같은 그룹임
+        
+        if(parent[a] > parent[b]){ // 높이 비교(음수)
             parent[b] += parent[a];
             parent[a] = b;
+        }else{
+            parent[a] += parent[b];
+            parent[b] = a;
         }
-        
     }
-    static int find(int x){
-        if(parent[x] < 0) return x;
+    
+    private static int find(int x){
+        if(parent[x] < 0) return x; // 이미 루트인 상태
         else{
             return parent[x] = find(parent[x]);
         }
-    }
+    } 
 }
