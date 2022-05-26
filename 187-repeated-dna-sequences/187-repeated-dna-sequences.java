f25debf078a455489ef526b98a30895a2a637834
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-26 22:07:35 +0900

    Time: 34 ms (52.75%), Space: 63.3 MB (52.34%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        // 반복되는 시퀀스들 반환
+        // 길이가 10인 반복되는 시퀀스들 반환
         HashSet<String> repeat = new HashSet<>();
-        HashSet<String> seen = new HashSet<>(); 
-        
-        for(int i = 0; i + 9 < s.length(); i++){
-            String seq = s.substring(i, i + 10);
-            if(!seen.add(seq)){ // 안넣어지는거면(false) 이미 한번 나왔다는 거임
-                repeat.add(seq);
+        HashSet<String> check = new HashSet<>();
+        for(int i = 0; i<s.length() - 9; i++){
+            String temp = s.substring(i, i + 10);
+            if(!check.add(temp)){
+                repeat.add(temp);
             }
-            
         }
         
-        return new ArrayList(repeat);
+        return new LinkedList<>(repeat);
     }
 }
