527b83d17aaae00d638c2c9e71bc4ba709384cfa
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-26 20:37:44 +0900

    Time: 1 ms (91.98%), Space: 54.3 MB (35.76%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,48 +1,33 @@
 class Solution {
-    static int n,m;
     public void setZeroes(int[][] matrix) {
-        // 0기준 +
-        n = matrix.length;
-        m = matrix[0].length;
-        
-        
+        // 배열의 0이 위치한 곳에서 상하좌우를 모두 0으로 채워준다.
+        int n = matrix.length;
+        int m = matrix[0].length;
         boolean isCol = false;
         
-        // step 1
         for(int i = 0; i<n; i++){
-            
             if(matrix[i][0] == 0) isCol = true;
-                
             for(int j = 1; j<m; j++){
-                if(matrix[i][j] == 0){ 
+                if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
-                    matrix[0][j] = 0; 
-                }    
-            }
-        }
-        
-        // step 2
-        for(int i = 1; i<n; i++){
-            for(int j= 1; j<m; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        // step 3
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+               if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0; 
+            }
+        }
+        
         if(matrix[0][0] == 0){
-            for(int i = 0; i<m; i++){
-                matrix[0][i] = 0; 
-            }
+            for(int i = 0; i<m; i++) matrix[0][i] = 0;
         }
         
-        
-        // step 4
         if(isCol){
-            for(int i = 0; i<n; i++){
-                matrix[i][0] = 0;
-            }
+            for(int i = 0; i<n; i++) matrix[i][0] = 0;
         }
+        
     }
 }
