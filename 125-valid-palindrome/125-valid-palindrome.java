de6ce86cd352f772e5f24f0c711c98991a88767b
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-27 15:51:40 +0900

    Time: 26 ms (35.46%), Space: 47.5 MB (18.09%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -3,9 +3,6 @@ class Solution {
         
         StringBuilder sb = new StringBuilder(s.toLowerCase().replaceAll("[^a-z0-9]", ""));
         
-        System.out.println(sb.toString());
-        System.out.println(sb.reverse().toString());
-        
         return sb.toString().equals(sb.reverse().toString());
     }
 }
