05a0dcac414697bc9e329404e941f1bd1b21727f
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-29 20:59:18 +0900

    Time: 0 ms (100.00%), Space: 42.8 MB (50.57%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -8,19 +8,18 @@ class Solution {
         
         for(int i = 0; i<n; i++){
             for(int j = i; j<m; j++){
-                int temp = 0;
-                temp = matrix[i][j];
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
         
+        
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m/2; j++){
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - 1 - j];
-                matrix[i][n - 1 - j] = temp;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m - j - 1];
+                matrix[i][m - j - 1] = temp;
             }
         }
     }
