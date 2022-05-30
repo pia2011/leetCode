23f4cde0f242df660a2052f181c4e94f23d15ae5
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-30 16:05:06 +0900

    Time: 7 ms (11.96%), Space: 44.7 MB (12.06%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        HashSet<List<Integer>> ansList = new HashSet<>();
+        // 가능한 모든 조합을 반환하면 됨
+        // 중복 허용 안함
+        
         Arrays.sort(nums);
-        backTracking(ansList, nums, 0, new LinkedList<>());
+        HashSet<List<Integer>> groups = new HashSet<>();
+        dfs(groups, nums, 0, new LinkedList<>());
         
-        return new LinkedList(ansList);
+        return new LinkedList<>(groups);
     }
-    private static void backTracking(HashSet<List<Integer>> ansList, int nums[], int start, List<Integer> list){
+    private static void dfs(HashSet<List<Integer>> groups ,int nums[], int start, List<Integer> list){
         
-        ansList.add(new LinkedList(list));
-        
+        groups.add(new LinkedList<>(list));
         for(int i = start; i<nums.length; i++){
             list.add(nums[i]);
-            backTracking(ansList, nums, i + 1, list);
+            dfs(groups, nums, i + 1, list);
             list.remove(list.size() - 1);
         }
-        
     }
 }
