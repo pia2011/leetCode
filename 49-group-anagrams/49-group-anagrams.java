976c36e73800bc6ba3562f676e5e9a62a5896bf8
gyuseong Kim <53935439+pia2011@users.noreply.github.com>
2022-05-31 16:01:35 +0900

    Time: 12 ms (59.02%), Space: 55.4 MB (63.24%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         // 같은 요소로 이루어진 단어들끼리 List로 묶어서 반환
-        HashMap<String, List<String>> hm = new HashMap<>();
+        HashMap<String, List<String>> groups = new HashMap<>();
+        
         for(String s : strs){
             char c[] = s.toCharArray();
             Arrays.sort(c);
-            String key = String.valueOf(c);
-            hm.putIfAbsent(key, new ArrayList<>());
-            hm.get(key).add(s);
+            String str = String.valueOf(c); // 공통
+            groups.putIfAbsent(str, new ArrayList<>());
+            groups.get(str).add(s);
         }
         
-        return new ArrayList<>(hm.values()); 
+        return new ArrayList<>(groups.values());
     }
 }
