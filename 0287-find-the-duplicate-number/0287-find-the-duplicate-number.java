7eeafbd65514b930f012b18a516f91be067c7716
김규성(LUKAS) <kks01650165@gmail.com>
2023-08-08 20:53:30 +0900

    Time: 34 ms (22.70%), Space: 56.1 MB (96.15%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -3,17 +3,29 @@ import java.util.*;
 class Solution {
     public int findDuplicate(int[] nums) {
         
-        int ans = 0;
+//         int ans = 0;
         
-        Set<Integer> hs = new HashSet<>();
+//         Set<Integer> hs = new HashSet<>();
+//         for(int num : nums){
+//             if(!hs.contains(num)){
+//                 hs.add(num);
+//             }else {
+//                 ans = num;
+//             }
+//         }        
+        
+//         return ans;
+        
+        int ans = -1;
+        int prev = -1;
+        Arrays.sort(nums);
         for(int num : nums){
-            if(!hs.contains(num)){
-                hs.add(num);
-            }else {
+            if(prev == num){ 
                 ans = num;
+                break;
             }
-        }        
-        
+            prev = num;
+        }
         
         return ans;
     }
