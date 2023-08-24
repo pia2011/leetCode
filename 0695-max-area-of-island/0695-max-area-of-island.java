9422637db53dfec9cd6884d18a69803bcf071ab7
김규성(LUKAS) <kks01650165@gmail.com>
2023-08-24 18:56:57 +0900

    Time: 2 ms (86.88%), Space: 43 MB (93.16%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,60 +1,30 @@
 class Solution {
-    // 가장 큰 섬의 면적을 구하라
-    
     private int n,m;
-    private boolean visited[][];
-    private int board[][];
     public int maxAreaOfIsland(int[][] grid) {
-        int ans = 0;
         n = grid.length;
         m = grid[0].length;
-        visited = new boolean[n][m];
-        board = grid;
-        
+        int ans = 0;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(visited[i][j] || grid[i][j] == 0) continue;
-                ans = Math.max(bfs(i,j), ans);
+                if(grid[i][j] == 0) continue;
+                ans = Math.max(ans, dfs(grid, i, j));
             }
         }
         
         return ans;
     }
     
-    private static final int dx[] = {0,1,0,-1};
-    private static final int dy[] = {1,0,-1,0};
-    private int bfs(int x, int y){
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(x,y));
-        visited[x][y] = true;
-        int ret = 1;
+    private int dfs(int grid[][], int x, int y){
+        if(x < 0 || y < 0 || x >= n || y >= m || grid[x][y] == 0) return 0;
+        grid[x][y] = 0;
         
-        while(!q.isEmpty()){
-            Point p = q.poll();
-            for(int i = 0; i<4; i++){
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if(!isIn(nx,ny)) continue;
-                if(board[nx][ny] == 0) continue;
-                if(visited[nx][ny]) continue;
-                ret++;
-                visited[nx][ny] = true;
-                q.add(new Point(nx,ny));
-            }
+        int ret = 1;
+        for(int i = 0; i<4; i++){
+            ret += dfs(grid, x + dx[i], y + dy[i]);
         }
         
         return ret;
     }
-    
-    private boolean isIn(int x, int y){
-        return x >= 0 && y >= 0 && x < n && y < m;
-    }
-    
-    private class Point{
-        int x, y;
-        Point(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+    private static final int dx[] = {0,1,0,-1};
+    private static final int dy[] = {1,0,-1,0};
 }
