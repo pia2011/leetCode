ead9aadefb0af556d39af3b9b222fe9413d8ecd5
김규성(LUKAS) <kks01650165@gmail.com>
2023-08-29 19:59:18 +0900

    Time: 3 ms (21.12%), Space: 41.3 MB (58.40%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+class Solution {
+    
+    // 전형적인 문제
+    // 주의 : 살아있으면 안됨
+    private int n,m;
+    private int board[][];
+    private int time[][];
+    private static final int INF = 987654321;
+    public int orangesRotting(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        board = grid;
+        time = new int[n][m];
+        
+        if(!isThereAnyFresh()) return 0;
+        if(!isThereAnyRotten()) return -1;
+        
+        for(int i = 0; i<n; i++) Arrays.fill(time[i], INF);
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(board[i][j] == 2 && time[i][j] == INF){ // 썩은 오렌지 && 최초 방문
+                    bfs(i,j);
+                }
+            }
+        }
+        
+        if(delCnt == 0) return 0;
+        else return isFreshOrangeExists() ? -1 : findMaxTime();
+    }
+    
+    private boolean isThereAnyFresh(){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(board[i][j] == 1) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    private boolean isThereAnyRotten(){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(board[i][j] == 2) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    private int findMaxTime(){
+        int ret = -1;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(time[i][j] == INF) continue;
+                ret = Math.max(ret, time[i][j]);
+            }
+        }
+        return ret;
+    }
+    private static int delCnt = 0;
+    private boolean isFreshOrangeExists(){ // 싱싱한 오렌지가 있는가
+        
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<m; j++){
+                if(time[i][j] == INF && board[i][j] == 1) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    private void bfs(int x, int y){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y, 0));
+        time[x][y] = 0;
+        delCnt++;
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            
+            for(int i = 0; i<4; i++){
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(!isIn(nx, ny)) continue;
+                if(time[nx][ny] <= p.cnt) continue;
+                if(board[nx][ny] == 1){ // 싱싱한 오렌지
+                    delCnt++;
+                    time[nx][ny] = p.cnt + 1;
+                    q.add(new Point(nx, ny, p.cnt + 1));    
+                }
+                
+            }
+        }
+    }
+    
+    private boolean isIn(int x, int y){
+        return x >= 0 && y >= 0 && x < n && y < m;
+    }
+    
+    private final static int dx[] = {0,1,0,-1};
+    private final static int dy[] = {1,0,-1,0};
+    
+    private class Point{
+        int x, y, cnt;
+        Point(int x, int y, int cnt){
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
+}
